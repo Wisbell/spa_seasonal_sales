6632d0ef4791be0de6331f512d2d4d076bddca50
Wisbell <westleyisbell@gmail.com>
2016-12-06 17:37:26 -0600

    Added select box functionality and cleaned up code

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,6 +1,6 @@
 'use strict'
 
-console.log("seasonal-sales.js Loaded");
+//console.log("seasonal-sales.js Loaded");
 
 // Store the products/department information
 var products;
@@ -27,14 +27,15 @@ function loadDepartments(event) {
 
     departments = JSON.parse(event.target.responseText);
 
-    console.log("departments", departments);
+    //console.log("departments", departments);
 
-    displayDepartments()
+    displayMain()
 }
 
 
 function addSeasonOptions(){
-    document.getElementById('seasonDiscounts').innerHTML = `<option>Autumn</option>
+    document.getElementById('seasonDiscounts').innerHTML = `<option>No Discount</option>
+                                                            <option>Autumn</option>
                                                             <option>Winter</option>
                                                             <option>Spring</option>`
 }
@@ -47,12 +48,64 @@ function displayDepartments(){
 
     for(var i = 0; i < departments.categories.length; i++) {
         //console.log(departments.categories[i])
-        console.log(departments.categories[i].name)
+        //console.log(departments.categories[i].name)
 
-        departmentDiv.insertAdjacentHTML('afterend', '');
+        departmentDiv.insertAdjacentHTML('afterbegin', `<div id="${departments.categories[i].name}">
+                                                        <h2>${departments.categories[i].name}</h2>
+                                                        </div>`);
+    }
+}
+
+
+function displayProducts(){
+    console.log("displayProducts function called")
+
+    for (var i = 0; i < products.products.length; i++){
+        //console.log(products.products[i])
+
+
+        //console.log("this should be an integer", products.products[i].category_id)
+
+        if(products.products[i].category_id === 1){
+            //console.log("this hsould be an integer", products.products.category_id)
+            document.getElementById("Apparel").insertAdjacentHTML('beforeend', `<p>${products.products[i].name}</p>
+                                                                                <p>${products.products[i].price}</p>`)
+
+        } else if (products.products[i].category_id === 2){
+            //console.log("this hsould be an integer", products.products.category_id)
+            document.getElementById("Furniture").insertAdjacentHTML('beforeend', `<p>${products.products[i].name}</p>
+                                                                                <p>${products.products[i].price}</p>`)
+
+        } else if (products.products[i].category_id === 3) {
+            //console.log("this hsould be an integer", products.products.category_id)
+            document.getElementById("Household").insertAdjacentHTML('beforeend', `<p>${products.products[i].name}</p>
+                                                                                <p>${products.products[i].price}</p>`)
+        } else {
+            console.log("Something broke")
+        }
     }
 
 
+}
+
+function seasonDiscounts(event){
+    console.log("seasonDiscounts function called")
+
+
+    console.log("event", event)
+
+    // create options on the DOM
+
+    var discountSelected = document.getElementById("seasonDiscounts").value;  // .selectedIndex?
+
+    console.log(discountSelected);
+
+}
+
+function displayMain() {
+    addSeasonOptions();
+    displayDepartments();
+    displayProducts();
 }
 
 
@@ -69,6 +122,8 @@ requestDepartments.addEventListener("load", loadDepartments);
 requestDepartments.open("GET", "categories.json");
 requestDepartments.send();
 
+// Event listener for select dropdown
+document.getElementById("seasonDiscounts").addEventListener('change', seasonDiscounts);
 
 // Testing Function
-addSeasonOptions();
+//addSeasonOptions();

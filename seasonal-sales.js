988d1266aded502fcb5a043f1ff00dd1b8070e17
Wisbell <westleyisbell@gmail.com>
2016-12-06 16:11:24 -0600

    Added event listeners

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,8 +1,10 @@
 'use strict'
 
-console.log("seasonal-sales.js Loaded")
+console.log("seasonal-sales.js Loaded");
 
-
+// Store the products/department information
+var products;
+var departments;
 
 
 function loadProducts(event) {
@@ -10,21 +12,63 @@ function loadProducts(event) {
 
     console.log("event", event)
 
-    var data = JSON.parse(event.target.responseText);
+    products = JSON.parse(event.target.responseText);
 
+    console.log("products", products)
 
-    console.log("data", data)
+    //displayDepartments()
 }
 
 
+function loadDepartments(event) {
+    console.log("loadDepartments function called!")
+
+    //console.log("event", event);
+
+    departments = JSON.parse(event.target.responseText);
+
+    console.log("departments", departments);
+
+    displayDepartments()
+}
 
 
+function addSeasonOptions(){
+    document.getElementById('seasonDiscounts').innerHTML = `<option>Autumn</option>
+                                                            <option>Winter</option>
+                                                            <option>Spring</option>`
+}
 
 
+function displayDepartments(){
+    console.log("displayDepartments function called!")
+
+    var departmentDiv = document.getElementById("departments");
+
+    for(var i = 0; i < departments.categories.length; i++) {
+        //console.log(departments.categories[i])
+        console.log(departments.categories[i].name)
+
+        departmentDiv.insertAdjacentHTML('afterend', '');
+    }
 
 
-var requestJson = new XMLHttpRequest();
+}
 
-requestJson.addEventListener("load", loadProducts);
-requestJson.open("GET", "products.json");
-requestJson.send();
+
+// XMLHTTP requests - get the json files
+
+var requestProducts = new XMLHttpRequest();
+
+requestProducts.addEventListener("load", loadProducts);
+requestProducts.open("GET", "products.json");
+requestProducts.send();
+
+var requestDepartments = new XMLHttpRequest();
+requestDepartments.addEventListener("load", loadDepartments);
+requestDepartments.open("GET", "categories.json");
+requestDepartments.send();
+
+
+// Testing Function
+addSeasonOptions();
